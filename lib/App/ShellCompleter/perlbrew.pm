41e40b43974a387e9b55d1af6d924567a8378b91
perlancar (@pc-home) <perlancar@gmail.com>
2016-04-30 16:52:58 +0700

    Release v0.001

---
+++ lib/App/ShellCompleter/perlbrew.pm
@@ -0,0 +1,16 @@
+package App::ShellCompleter::perlbrew;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Shell completion for perlbrew
+
+=head1 SYNOPSIS
+
+See L<_perlbrew> included in this distribution.
+
+
+=head1 SEE ALSO
+
+L<App::perlbrew>.
