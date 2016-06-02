bc21b30d1c8ff64b45a74d810d1d721d63bf3a93
perlancar (@pc-office) <perlancar@gmail.com>
2016-06-02 20:16:11 +0700

    [refactor] Move routine to .pm file so it can be reused by other dists

--- lib/App/ShellCompleter/perlbrew.pm
+++ lib/App/ShellCompleter/perlbrew.pm
@@ -3,6 +3,144 @@ package App::ShellCompleter::perlbrew;
 # DATE
 # VERSION
 
+use 5.010001;
+use strict;
+use warnings;
+
+use Complete::Util qw(complete_array_elem);
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(
+                       complete_perl_available_to_install
+                       complete_perl_installed_to_use
+                       complete_perl_installed_name
+                       complete_perl_alias
+                       list_available_perls
+                       list_available_perl_versions
+                       list_installed_perls
+                       list_installed_perl_versions
+                       list_perl_libs
+                       list_perl_aliases
+               );
+
+sub list_available_perls {
+    require File::Spec;
+    require File::Slurper;
+    my $tmp_path = File::Spec->tmpdir() . "/_perlbrew_available_perls.tmp";
+    unless ((-f $tmp_path) && (-M _) <= 1) {
+        File::Slurper::write_text($tmp_path, scalar `perlbrew available`);
+    }
+    my $available = File::Slurper::read_text($tmp_path);
+    my @res;
+    for (split /^/, $available) {
+        s/^[i ] //;
+        chomp;
+        push @res, $_;
+    }
+    @res;
+}
+
+sub list_available_perl_versions {
+    my @res;
+    for (list_available_perls()) {
+        s/\D+(?=\d)//;
+        push @res, $_;
+    }
+    @res;
+}
+
+sub list_installed_perls {
+    my @res;
+    for (split /^/, `perlbrew list`) {
+        s/^[* ] //;
+        s/ \(.+\)$//; # alias
+        chomp;
+        push @res, $_;
+    }
+    @res;
+}
+
+sub list_installed_perl_versions {
+    my @res;
+    for (list_installed_perls()) {
+        next unless /\d/;
+        s/\D+(?=\d)//;
+        push @res, $_;
+    }
+    @res;
+}
+
+sub list_perl_aliases {
+    my @res;
+    for (split /^/, `perlbrew list`) {
+        s/^[* ] //;
+        s/ \(.+\)$// or next; # alias
+        chomp;
+        push @res, $_;
+    }
+    @res;
+}
+
+sub list_perl_libs {
+    my @res;
+    for (split /^/, `perlbrew lib list`) {
+        chomp;
+        push @res, $_;
+    }
+    @res;
+}
+
+sub complete_perl_available_to_install {
+    my $word = shift;
+
+    local $Complete::Common::OPT_FUZZY = 0;
+    complete_array_elem(
+        word => $word,
+        array => [
+            ( list_available_perls(),
+              "perl-stable", "stable",
+              "perl-blead", "blead" ) x
+                  ($word =~ /^\D|^$/ ? 1:0),
+            list_available_perl_versions(),
+        ],
+    );
+}
+
+sub complete_perl_installed_to_use {
+    my $word = shift;
+
+    local $Complete::Common::OPT_FUZZY = 0;
+    complete_array_elem(
+        word => $word,
+        array => [
+            ( list_installed_perls() ) x
+                ($word =~ /^\D|^$/ ? 1:0),
+            list_installed_perl_versions(),
+        ],
+    );
+}
+
+sub complete_perl_installed_name {
+    my $word = shift;
+    local $Complete::Common::OPT_FUZZY = 0;
+    return complete_array_elem(
+        word => $word,
+        array => [list_installed_perls()],
+    );
+}
+
+sub complete_perl_alias {
+    my $word = shift;
+
+    local $Complete::Common::OPT_FUZZY = 0;
+    complete_array_elem(
+        word => $word,
+        array => [
+            list_perl_aliases(),
+        ],
+    );
+}
+
 1;
 # ABSTRACT: Shell completion for perlbrew
 
